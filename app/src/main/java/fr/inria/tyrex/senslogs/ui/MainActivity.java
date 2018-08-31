a5e7ebec770c55494481783af5b994236c36fe2f
Thibaud <thibaud.michel@gmail.com>
2018-08-31 11:45:47 +0200

    Fixed app title issue

--- app/src/main/java/fr/inria/tyrex/senslogs/ui/MainActivity.java
+++ app/src/main/java/fr/inria/tyrex/senslogs/ui/MainActivity.java
@@ -1,13 +1,21 @@
 package fr.inria.tyrex.senslogs.ui;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 
+import fr.inria.tyrex.senslogs.R;
 import fr.inria.tyrex.senslogs.ui.utils.SingleFragmentActivity;
 
 /**
  * This activity handles only {@link MainFragment}
  */
 public class MainActivity extends SingleFragmentActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().setTitle(R.string.activity_main);
+    }
 
     @Override
     protected Fragment createFragment() {
